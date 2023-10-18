1ebe786838aed0b6d4a89dc6185dd10e28aecaf7
Megabyte21 <roelrivera0176@gmail.com>
2023-10-18 15:58:38 -0500

    Added more robust config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import './helpers/dotenv'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+import logger from './helpers/logger'
+import router from './routes'
+import { notFound, errorHandler } from './helpers/errors'
+
+const port = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
 
-const port = 3000
+app.use(morgan(process.env.MORGAN_LOG))
+app.use(cors({ origin: process.env.ORIGIN }))
+app.use(helmet())
 
-app.get('/', (req, res) => {
-  res.send({ msg: 'Hello There' })
-})
+app.use(router)
 
-app.listen(port)
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () =>
+  logger.info(`Application Started at http://localhost:${process.env.PORT}`),
+)
